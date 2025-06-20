55bd9fba1ccc9fcfedd904c457c7f3b2dfd43c82
youssefosama98 <youssef.osama@scandiweb.com>
2025-06-20 11:23:47 +0300

    Revton_task - create Revton_OrderView Module

---
+++ app/code/Revton/OrderView/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category  Revton
+ * @package   Revton_OrderView
+ * @author    Youssef Osama <youssef.osama.fareed@gmail.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Revton_OrderView',
+    __DIR__
+);
